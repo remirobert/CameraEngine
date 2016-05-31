3c0c18b6febfa96ca23065076df8917d8712c2f6
eyaldar <eyaldar@gmail.com>
2016-05-31 11:07:16 +0300

    Added public accessor to previously added properies

--- CameraEngine/CameraEngine.swift
+++ CameraEngine/CameraEngine.swift
@@ -204,7 +204,7 @@ public class CameraEngine: NSObject {
         }
     }
     
-    var isAdjustingFocus: Bool {
+    public var isAdjustingFocus: Bool {
         get {
             if let `captureDevice` = captureDevice {
                 return captureDevice.adjustingFocus
@@ -214,7 +214,7 @@ public class CameraEngine: NSObject {
         }
     }
     
-    var isAdjustingExposure: Bool {
+    public var isAdjustingExposure: Bool {
         get {
             if let `captureDevice` = captureDevice {
                 return captureDevice.adjustingExposure
@@ -224,7 +224,7 @@ public class CameraEngine: NSObject {
         }
     }
     
-    var isAdjustingWhiteBalance: Bool {
+    public var isAdjustingWhiteBalance: Bool {
         get {
             if let `captureDevice` = captureDevice {
                 return captureDevice.adjustingWhiteBalance
